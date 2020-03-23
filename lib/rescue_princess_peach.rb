799f770d09168d94eb71a25ace7bbc5746a131df
Arynn Boniface <arynn.boniface@gmail.com>
2020-03-23 18:35:37 +0000

    Automatically backed up by Learn

--- lib/rescue_princess_peach.rb
+++ lib/rescue_princess_peach.rb
@@ -1,11 +1,9 @@
 def rescue_princess_peach
   status = "rescued"
   puts "Hooray! Mario has rescued Princess Peach."
-  rescue_princess_peach
-  puts status
 end
 
 ## Comment the below back in to see method scope in action!
 
-# rescue_princess_peach
-# puts status
+rescue_princess_peach
+puts status
